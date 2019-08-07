ced68550c7b9ea0e1a6c8d351e6b2303cdedfb85
theresa <tdiff@umd.edu>
2019-08-07 17:03:50 -0400

    updated BWI through aug 7

--- BWI-Historical-Weather-Analysis.R
+++ BWI-Historical-Weather-Analysis.R
@@ -15,6 +15,7 @@ install.packages("weathermetrics")
 install.packages("ggplot2")
 install.packages("reshape")
 install.packages("writexl")
+install.packages("purrr")
 
 library(tidyverse)
 library(stringr)
@@ -26,6 +27,8 @@ library(ggplot2)
 library(reshape)
 library(writexl)
 
+rm(list=ls())
+
 ##############################
 ##### LOAD DATA ##############
 ##############################
@@ -36,12 +39,15 @@ bwi_data <- read_csv("/Users/tdiff/Documents/Github/bwi_dmh_temps/old-data/balti
 # Load Baltimore Inner Harbor Temperature Data
 inner_harbor_data <- read_excel("/Users/tdiff/Documents/Github/bwi_dmh_temps/old-data/baltimore-inner-harbor-temperature/DMH.xlsx")
 
+#Load new inner harbor data for July 4-30
+new_inner_harbor_data <- read_csv("/Users/tdiff/Documents/Github/bwi_dmh_temps/old-data/DMH.txt")
+
 ##############################
 ##### CLEAN DATA #############
 ##############################
 
 # Clean BWI data 
-bwi_data <- bwi_data %>%
+clean_bwi_data <- bwi_data %>%
   filter(tmpf != 'M', dwpf != 'M') %>%
   mutate(tmpf = as.numeric(tmpf),
          dwpf = as.numeric(dwpf)) %>%
@@ -64,9 +70,33 @@ bwi_data <- bwi_data %>%
   filter(!is.na(avg_hourly_relative_humidity_bwi)) %>%
   distinct()
 
-# Clean Inner Harbor data 
+###Save newly clean BWI data and read it in
+write.csv(clean_bwi_data, "/Users/tdiff/Documents/Github/bwi_dmh_temps/clean_bwi_data.csv")
+clean_bwi_data <- read.csv("/Users/tdiff/Documents/Github/bwi_dmh_temps/clean_bwi_data.csv") %>%
+  mutate(date = as.Date(date, format="%Y-%m-%d"))
 
-inner_harbor_data <- inner_harbor_data %>%
+#######
+############Clean Inner Harbor data 
+#######
+
+#Rename new inner harbor columns and change inner harbor columns to numerics so can append new inner harbor data to old
+renamed_new_inner_harbor_data <- new_inner_harbor_data %>%
+  select(-feel) %>%
+  dplyr::rename(LOCATION = station,
+         DATETIME = valid,
+         TEMPERATURE = tmpf,
+         DEW_POINT = dwpf,
+         RELATIVE_HUMIDITY = relh)
+
+numeric_inner_harbor_data <- inner_harbor_data %>%
+  mutate(TEMPERATURE = as.numeric(TEMPERATURE),
+         DEW_POINT = as.numeric(DEW_POINT),
+         RELATIVE_HUMIDITY = as.numeric(RELATIVE_HUMIDITY))
+  
+bind_inner_harbor_data <- 
+  dplyr::bind_rows(numeric_inner_harbor_data, renamed_new_inner_harbor_data) 
+
+clean_inner_harbor_data <- bind_inner_harbor_data %>%
   filter(TEMPERATURE != 'M', DEW_POINT != 'M') %>%
   mutate(TEMPERATURE = as.numeric(TEMPERATURE),
          DEW_POINT = as.numeric(DEW_POINT)) %>%
@@ -89,36 +119,10 @@ inner_harbor_data <- inner_harbor_data %>%
   filter(!is.na(avg_hourly_relative_humidity_dmh)) %>%
   distinct()
 
-#Read and clean new inner harbor data for July 4-30
-new_inner_harbor_data <- read_csv("/Users/tdiff/Documents/Github/bwi_dmh_temps/old-data/DMH.txt")
-
-new_inner_harbor_data <- new_inner_harbor_data %>%
-  filter(tmpf != 'M', dwpf != 'M') %>%
-  mutate(tmpf = as.numeric(tmpf),
-         dwpf = as.numeric(dwpf)) %>%
-  mutate(date = as.Date(valid, format="%Y-%m-%d")) %>%
-  mutate(year=year(valid)) %>%
-  mutate(month=month(valid)) %>%
-  mutate(hour=hour(valid)) %>%
-  mutate(day=day(valid)) %>%
-  mutate(hour=hour(valid)) %>%
-  distinct(valid, .keep_all = TRUE) %>%
-  mutate(heat_index = heat.index(t=tmpf, dp=dwpf, temperature.metric = "fahrenheit", round=0)) %>%
-  mutate(relative_humidity = dewpoint.to.humidity(dp = dwpf, t = tmpf, temperature.metric = "fahrenheit")) %>%
-  select(date, year, month, day, hour, tmpf, dwpf, relative_humidity, heat_index) %>%
-  group_by(date, year, month, day, hour) %>%
-  summarise(avg_hourly_temperature_dmh = mean(tmpf),
-            avg_hourly_dewpoint_dmh = mean(dwpf),
-            avg_hourly_relative_humidity_dmh = mean(relative_humidity),
-            avg_hourly_heat_index_dmh = mean(heat_index)
-  ) %>%
-  filter(!is.na(avg_hourly_relative_humidity_dmh)) %>%
-  distinct()
- 
-#Append new inner harbor data to old
-clean_inner_harbor_data <-
-  dplyr::bind_rows(clean_inner_harbor_data, new_inner_harbor_data) %>%
-  select(-X)
+####Save and load clean inner harbor data
+write.csv(clean_inner_harbor_data, "/Users/tdiff/Documents/Github/bwi_dmh_temps/clean_inner_harbor_data.csv")
+clean_inner_harbor_data <- read.csv("/Users/tdiff/Documents/Github/bwi_dmh_temps/clean_inner_harbor_data.csv") %>%
+  mutate(date = as.Date(date, format="%Y-%m-%d"))
 
 ###########################################
 ###### Adjust Inner Harbor Temperatures####
@@ -128,7 +132,7 @@ clean_inner_harbor_data <-
 # We have 20 years of dew point and temperature data for DMH (Inner Harbor), which allows us to calculate heat index. 
 # I talked with Dan Li, a climate researcher at Boston University who has studied variation in urban heat island temperatures between suburbs in different seasons and different hours of the day, who said a pretty good way to calculate the difference would be this:
 # 1. Join the 20 years of DMH data to the 20 years of BWI data.  Join on date and hour, so we end up with a data frame of values for both DMH and BWI for each date and hour in our data set.  Do an inner join, so that if for some reason DMH is missing an hour from its data set (or vice versa), it doesn't mess up our averages. Calculate new columns with difference in temperature, dew point, relative humidity and heat index between BWI and DMH.  Since in almost all cases BWI will be colder than DMH, make it so the difference is positive if DMH is greater than BWI.
-dmhbwi20_w <- 
+dmhbwi <- 
   inner_join(clean_bwi_data, clean_inner_harbor_data, by = c("date", "hour")) %>%
   dplyr::rename(year = year.x, month = month.x, day = day.x) %>%
   select(-year.y, -month.y, -day.y) %>%
@@ -138,7 +142,7 @@ dmhbwi20_w <-
          hiDiff = avg_hourly_heat_index_dmh - avg_hourly_heat_index_bwi) 
 
 # 4. Group by month and hour and calculate the AVERAGE difference in temperature, dew point, relative humidity and heat index. 
-summarized_join <- dmhbwi20_w %>%
+summarized_join <- dmhbwi %>%
   group_by(month, hour) %>%
   summarize(avgTempDiff = mean(tempDiff),
          avgDewDiff = mean(dewDiff),
@@ -146,7 +150,7 @@ summarized_join <- dmhbwi20_w %>%
          avgHiDiff = mean(hiDiff))
   
 # 5. Go back to the original 75 years of BWI data (after it's been cleaned, not on raw import).  Write case when function adjusting the temperature and dew point values based on table created in step 4. Create two new columns to recalculate heat index and relative humidity (see cleaning function above, weathermetrics package has a nice way of doing this). Call them adjusted_heat_index, adjusted_relative_humidity 
-join_bwi_data_w <-
+join_bwi_data <-
  inner_join(summarized_join, clean_bwi_data, by = c("month", "hour")) %>%
   mutate(adjusted_temp = avg_hourly_temperature_bwi + avgTempDiff,
          adjusted_dew = avg_hourly_dewpoint_bwi + avgDewDiff,
@@ -154,10 +158,10 @@ join_bwi_data_w <-
          adjusted_relative_humidity = dewpoint.to.humidity(dp = adjusted_dew, t = adjusted_temp, temperature.metric = "fahrenheit"))
 
 #Take new adjusted columns and put into a new table as estimates for inner harbor data
-estimated_inner_harbor_data_w <- join_bwi_data_w %>%
+estimated_inner_harbor_data <- join_bwi_data %>%
   select(-avgTempDiff, -avgDewDiff, -avgRhDiff, -avgHiDiff, -avg_hourly_temperature_bwi, 
-         -avg_hourly_dewpoint_bwi, -avg_hourly_relative_humidity_bwi, -avg_hourly_heat_index_bwi) %>%
-  select(-X)
+         -avg_hourly_dewpoint_bwi, -avg_hourly_relative_humidity_bwi, -avg_hourly_heat_index_bwi) 
+# %>% select(-X)
 
 #Check estimated data against cleaned inner harbor data
 compare_inner_harbor_data <- estimated_inner_harbor_data %>%
@@ -175,34 +179,36 @@ summarized_compare <- compare_inner_harbor_data %>%
             avg_difference_rh = mean(difference_rh),
             avg_difference_ri = mean(difference_hi))
 
-###########################################
-###### Save and load files here ##########################
-###########################################
-#Save cleaned files for easy loading
-clean_bwi_data <- write.csv(bwi_data, "/Users/tdiff/Documents/Github/bwi_dmh_temps/clean_bwi_data.csv")
-clean_inner_harbor_data <- write.csv(clean_inner_harbor_data, "/Users/tdiff/Documents/Github/bwi_dmh_temps/clean_inner_harbor_data.csv")
-#Save newly calculated/estimated inner harbor data going back 70 years
-estimated_inner_harbor_data <- write.csv(estimated_inner_harbor_data_w, "/Users/tdiff/Documents/Github/bwi_dmh_temps/estimated_inner_harbor_data.csv")
+##join estimated inner harbor data with inner harbor data updated through 7-30-19
+updated_estimated_inner_harbor_data <- estimated_inner_harbor_data %>%
+  dplyr::rename(avg_hourly_temperature_dmh = adjusted_temp,
+         avg_hourly_dewpoint_dmh = adjusted_dew,
+         avg_hourly_relative_humidity_dmh = adjusted_relative_humidity,
+         avg_hourly_heat_index_dmh = adjusted_heat_index) 
+        # %>%  select(-X)
 
-##Load cleaned files
-clean_bwi_data <- read.csv("/Users/tdiff/Documents/Github/bwi_dmh_temps/clean_bwi_data.csv") %>%
-  mutate(date = as.Date(date, format="%Y-%m-%d"))
-clean_inner_harbor_data <- read.csv("/Users/tdiff/Documents/Github/bwi_dmh_temps/clean_inner_harbor_data.csv") %>%
+updated_estimated_inner_harbor_data <- 
+  full_join(updated_estimated_inner_harbor_data, clean_inner_harbor_data) %>%
+  arrange(date, hour)
+
+#Save inner harbor data updated through 7-30-2019
+write.csv(updated_estimated_inner_harbor_data, "/Users/tdiff/Documents/Github/bwi_dmh_temps/updated_estimated_inner_harbor_data.csv")
+
+updated_estimated_inner_harbor_data <- read.csv("/Users/tdiff/Documents/Github/bwi_dmh_temps/updated_estimated_inner_harbor_data.csv") %>%
   mutate(date = as.Date(date, format="%Y-%m-%d")) %>%
   select(-X)
-estimated_inner_harbor_data <- read.csv("/Users/tdiff/Documents/Github/bwi_dmh_temps/estimated_inner_harbor_data.csv") %>%
-  mutate(date = as.Date(date, format="%Y-%m-%d"))
 
 ###########################################
 ###### QUESTIONS ##########################
 ###########################################
+
 #FOR INNER HARBOR: What have the patterns been with 90+, 103+ heat index days? What years have had the most of these days? What have the longest stretches been? Are there more recently that have been intense?
-hi_over_ninety_dmh <- estimated_inner_harbor_data %>%
-  select(-X.1, -X) %>%
-  filter(adjusted_heat_index >= 90) %>%
+hi_over_ninety_dmh <- updated_estimated_inner_harbor_data %>%
+  filter(avg_hourly_heat_index_dmh >= 90) %>%
   distinct(date, .keep_all=TRUE) %>%
-  group_by(year, month) %>%
-  summarise(count=n())
+  group_by(year) %>%
+  summarise(count=n()) #%>%
+  #summarise(average_days = mean(count))
   
 ggplot(data = hi_over_ninety_dmh, aes(x=year, y=count)) + 
   geom_col() + 
@@ -218,9 +224,8 @@ ggsave(filename = "dmh_days_heat_index_over_ninety.png",
        width = 20, height = 15, units = "in")
 
 ###similar to above but for hours instead of days
-hi_over_ninety_dmh <- estimated_inner_harbor_data %>%
-  select(-X.1, -X) %>%
-  filter(adjusted_heat_index >= 90) %>%
+hi_over_ninety_dmh <- updated_estimated_inner_harbor_data %>%
+  filter(avg_hourly_heat_index_dmh >= 90) %>%
   group_by(year) %>%
   summarise(count=n())
 
@@ -237,21 +242,46 @@ ggsave(filename = "dmh_hours_heat_index_over_ninety.png",
        device = "png", path = "/Users/tdiff/Documents/Github/bwi_dmh_temps/Graphs",
        width = 20, height = 15, units = "in")
 
-#Inner harbor heat index >= 103
-hi_over_103_dmh <- estimated_inner_harbor_data %>%
-  select(-X.1, -X) %>%
-  filter(adjusted_heat_index >= 103) %>%
+#Inner harbor heat index >= 100
+hi_over_100_dmh <- updated_estimated_inner_harbor_data %>%
+  filter(avg_hourly_heat_index_dmh >= 100) %>%
   distinct(date, .keep_all=TRUE) %>%
   group_by(year) %>%
-  summarise(count=n())
+  filter(year > 1980,
+         year < 2013) %>%
+  summarise(count=n()) #%>% 
+  #summarise(avg_days_per_year = mean(count))
 
-ggplot(data = hi_over_103_dmh, aes(x=year, y=count)) + 
+#write_xlsx(hi_over_100_dmh, "/Users/tdiff/Documents/Github/bwi_dmh_temps/data-for-others/hi_over_100_dmh.xlsx")
+
+ggplot(data = hi_over_100_dmh, aes(x=year, y=count)) + 
   geom_col() + 
   geom_smooth() + 
   theme(plot.title = element_text(size = 25),
         axis.title = element_text(size=20),
         axis.text = element_text(size=15)) +
-  labs(title = "#Days When Heat Index > 103 in Inner Harbor",
+  labs(title = "#Days When Heat Index > 100 in Inner Harbor",
+       x = "Year",
+       y = "Days")
+ggsave(filename = "dmh_days_heat_index_over_100.png",
+       device = "png", path = "/Users/tdiff/Documents/Github/bwi_dmh_temps/Graphs",
+       width = 20, height = 15, units = "in")
+
+#Inner harbor heat index >= 105
+hi_over_105_dmh <- updated_estimated_inner_harbor_data %>%
+  filter(avg_hourly_heat_index_dmh >= 105) %>%
+  distinct(date, .keep_all=TRUE) %>%
+  group_by(year) %>%
+  summarise(count=n()) #%>%
+  #summarise(average_days = mean(count))
+
+ggplot(data = hi_over_105_dmh, aes(x=year, y=count)) + 
+  geom_col() + 
+  geom_smooth() + 
+  theme(plot.title = element_text(size = 25),
+        axis.title = element_text(size=20),
+        axis.text = element_text(size=15)) +
+  labs(title = "#Days When Heat Index > 105 in Inner Harbor",
        x = "Year",
        y = "Days")
 ggsave(filename = "dmh_days_heat_index_over_103.png",
@@ -259,9 +289,8 @@ ggsave(filename = "dmh_days_heat_index_over_103.png",
        width = 20, height = 15, units = "in")
 
 #same as above but for hours, not days
-hi_over_103_dmh <- estimated_inner_harbor_data %>%
-  select(-X.1, -X) %>%
-  filter(adjusted_heat_index >= 103) %>%
+hi_over_103_dmh <- updated_estimated_inner_harbor_data %>%
+  filter(avg_hourly_heat_index_dmh >= 103) %>%
   group_by(year, month) %>%
   summarise(count=n())
 
@@ -283,11 +312,12 @@ ggsave(filename = "dmh_hours_heat_index_over_103.png",
 ####BWI DATA####
 #BWI over 90 data
 hi_over_ninety_bwi <- clean_bwi_data %>%
-  select(-X) %>%
+  #  select(-X) %>%
   filter(avg_hourly_heat_index_bwi >= 90) %>%
   distinct(date, .keep_all=TRUE) %>%
   group_by(year) %>%
-  summarise(count=n())
+  summarise(count=n()) #%>%
+  #summarise(average_days = mean(count))
 
 ggplot(data = hi_over_ninety_bwi, aes(x=year, y=count)) + 
   geom_col() + 
@@ -322,24 +352,47 @@ ggsave(filename = "bwi_hours_heat_index_over_ninety.png",
        device = "png", path = "/Users/tdiff/Documents/Github/bwi_dmh_temps/Graphs",
        width = 20, height = 15, units = "in")
 
-#BWI over 103 data
-hi_over_103_bwi <- clean_bwi_data %>%
+#BWI over 100 data
+hi_over_100_bwi <- clean_bwi_data %>%
   select(-X) %>%
-  filter(avg_hourly_heat_index_bwi >= 103) %>%
+  filter(avg_hourly_heat_index_bwi >= 100) %>%
   distinct(date, .keep_all=TRUE) %>%
-  group_by(year, month) %>%
-  summarise(count=n())
+  group_by(year) %>%
+  summarise(count=n()) #%>%
+  #summarise(average_days = mean(count))
 
-ggplot(data = hi_over_103_bwi, aes(x=year, y=count)) + 
+ggplot(data = hi_over_100_bwi, aes(x=year, y=count)) + 
   geom_col() + 
   geom_smooth() + 
   theme(plot.title = element_text(size = 25),
         axis.title = element_text(size=20),
         axis.text = element_text(size=15)) +
-  labs(title = "#Days When Heat Index > 103 in BWI",
+  labs(title = "#Days When Heat Index > 100 in BWI",
        x = "Year",
        y = "Days")
-ggsave(filename = "bwi_days_heat_index_over_103.png",
+ggsave(filename = "bwi_days_heat_index_over_100.png",
+       device = "png", path = "/Users/tdiff/Documents/Github/bwi_dmh_temps/Graphs",
+       width = 20, height = 15, units = "in")
+
+#BWI over 105 data
+hi_over_105_bwi <- clean_bwi_data %>%
+  select(-X) %>%
+  filter(avg_hourly_heat_index_bwi >= 105) %>%
+  distinct(date, .keep_all=TRUE) %>%
+  group_by(year) %>%
+  summarise(count=n()) #%>%
+  #summarise(average_days = mean(count))
+
+ggplot(data = hi_over_105_bwi, aes(x=year, y=count)) + 
+  geom_col() + 
+  geom_smooth() + 
+  theme(plot.title = element_text(size = 25),
+        axis.title = element_text(size=20),
+        axis.text = element_text(size=15)) +
+  labs(title = "#Days When Heat Index > 105 in BWI",
+       x = "Year",
+       y = "Days")
+ggsave(filename = "bwi_days_heat_index_over_105.png",
        device = "png", path = "/Users/tdiff/Documents/Github/bwi_dmh_temps/Graphs",
        width = 20, height = 15, units = "in")
 
@@ -365,17 +418,17 @@ ggsave(filename = "bwi_hours_heat_index_over_103.png",
 
 #Inner harbor data for July/August: Have the hottest months of the year (July, August) gotten more hot on average?
 #done by building a multi-line graph by making months into separate columns, joining, and then plotting; geom_smooth lines are easier to read than geom_line
-july_dmh <- estimated_inner_harbor_data %>%
+july_dmh <- updated_estimated_inner_harbor_data %>%
   filter(month == 7,
-         adjusted_heat_index != "NA") %>%
+         avg_hourly_heat_index_dmh != "NA") %>%
   group_by(year, month) %>%
-  summarise(monthly_average_heat_index_july = mean(adjusted_heat_index))
+  summarise(monthly_average_heat_index_july = mean(avg_hourly_heat_index_dmh))
 
-august_dmh <- estimated_inner_harbor_data %>%
+august_dmh <- updated_estimated_inner_harbor_data %>%
   filter(month ==8,
-         adjusted_heat_index != "NA") %>%
+         avg_hourly_heat_index_dmh != "NA") %>%
   group_by(year, month) %>%
-  summarise(monthly_average_heat_index_august = mean(adjusted_heat_index))
+  summarise(monthly_average_heat_index_august = mean(avg_hourly_heat_index_dmh))
 
 july_august_dmh <-
   inner_join(july_dmh, august_dmh, by="year")
@@ -448,9 +501,9 @@ ggsave(filename = "bwi_days_above_80_col.png",
        width = 20, height = 15, units = "in")
 
 #Data for dmh
-dmh_days_above_80 <- estimated_inner_harbor_data %>%
+dmh_days_above_80 <- updated_estimated_inner_harbor_data %>%
   group_by(year, date) %>%
-  summarise(min_heat_index = min(adjusted_heat_index)) %>%
+  summarise(min_heat_index = min(avg_hourly_heat_index_dmh)) %>%
   filter(min_heat_index >= 80) %>%
   summarise(dmh_days=n())
 #count is 322
@@ -646,3 +699,30 @@ write_xlsx(adam_inner_harbor_data_july_sixteen, "/Users/tdiff/Documents/Github/b
 sean_june_july_inner_harbor_data <- clean_inner_harbor_data %>%
   filter(date > "2019-05-31")
 write_xlsx(sean_june_july_inner_harbor_data, "/Users/tdiff/Documents/Github/bwi_dmh_temps/data-for-others/sean_june_july_inner_harbor_data.xlsx")
+
+#read in and clean updated BWI data
+clean_updated_bwi_data <- 
+  read_csv("/Users/tdiff/Documents/Github/bwi_dmh_temps/BWI.txt") %>%
+  filter(tmpf != 'M', dwpf != 'M') %>%
+  mutate(tmpf = as.numeric(tmpf),
+         dwpf = as.numeric(dwpf)) %>%
+  mutate(date = as.Date(valid, format="%Y-%m-%d")) %>%
+  mutate(year=year(valid)) %>%
+  mutate(month=month(valid)) %>%
+  mutate(hour=hour(valid)) %>%
+  mutate(day=day(valid)) %>%
+  mutate(hour=hour(valid)) %>%
+  distinct(valid, .keep_all = TRUE) %>%
+  mutate(heat_index = heat.index(t=tmpf, dp=dwpf, temperature.metric = "fahrenheit", round=0)) %>%
+  mutate(relative_humidity = dewpoint.to.humidity(dp = dwpf, t = tmpf, temperature.metric = "fahrenheit")) %>%
+  select(date, year, month, day, hour, tmpf, dwpf, relative_humidity, heat_index) %>%
+  group_by(date, year, month, day, hour) %>%
+  summarise(avg_hourly_temperature_bwi = mean(tmpf),
+            avg_hourly_dewpoint_bwi = mean(dwpf),
+            avg_hourly_relative_humidity_bwi = mean(relative_humidity),
+            avg_hourly_heat_index_bwi = mean(heat_index)
+  ) %>%
+  filter(!is.na(avg_hourly_relative_humidity_bwi)) %>%
+  distinct()
+
+write_xlsx(clean_updated_bwi_data, "/Users/tdiff/Documents/Github/bwi_dmh_temps/clean_updated_bwi_data.xlsx")
